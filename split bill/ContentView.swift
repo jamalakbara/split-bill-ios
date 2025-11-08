65820a08d5f237eab7398bd470aaed4650040f40
akbar <jamalakbaralam@gmail.com>
2025-11-09 03:36:53 +0700

    Initial Commit

---
+++ split bill/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  split bill
+//
+//  Created by Chairil Akbar on 09/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ split bill/split_billApp.swift
@@ -0,0 +1,17 @@
+//
+//  split_billApp.swift
+//  split bill
+//
+//  Created by Chairil Akbar on 09/11/25.
+//
+
+import SwiftUI
+
+@main
+struct split_billApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
